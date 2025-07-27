b310633628171b98772a54abbb6c7a405471e30a
alonsocontr <alonsocontreras007@gmail.com>
2025-07-27 15:57:23 -0700

    fixed SQL details for production

--- app/database.py
+++ app/database.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
 # PostgreSQL login details pulled from .env
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if not SQLALCHEMY_DATABASE_URL:
