f326b68dfd4aa8459e190924d07548220d6c9a6d
alonsocontr <alonsocontreras007@gmail.com>
2025-07-27 15:52:48 -0700

    updated environmental variable handling

--- app/database.py
+++ app/database.py
@@ -5,8 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
 # Load environmental variables from .env
-env_path = Path(__file__).resolve().parent.parent / ".env"
-load_dotenv(dotenv_path=env_path)
+load_dotenv()
 
 # PostgreSQL login details pulled from .env
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
