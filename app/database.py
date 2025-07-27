c4440dbe6452dcf3cb0f0937ccfce55a6cddd3f7
alonsocontr <alonsocontreras007@gmail.com>
2025-07-27 15:57:05 -0700

    fixed SQL details for production

--- app/database.py
+++ app/database.py
@@ -2,13 +2,12 @@ import os
 from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 
-# Load environmental variables from .env
-load_dotenv()
 
 # PostgreSQL login details pulled from .env
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+if not SQLALCHEMY_DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable is not set")
 
 # Connect to database using login details
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
