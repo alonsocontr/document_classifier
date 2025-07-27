ee766a5a0b4dc4253551246b923669a306339111
alonsocontr <alonsocontreras007@gmail.com>
2025-07-27 15:45:30 -0700

    updated SQL login redundancies

--- app/database.py
+++ app/database.py
@@ -9,17 +9,11 @@ env_path = Path(__file__).resolve().parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 # PostgreSQL login details pulled from .env
-DATABASE_URL = os.getenv("DATABASE_URL", "url")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "user")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "document_classifier")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Connect to database using login details
-SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-# Create engine
+# Create engine and base for models
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
